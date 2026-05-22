0a4bd2add9125d864eb08252910febc177bb158c
Edward Mironov <edwardmir0515@gmail.com>
2026-05-22 16:10:25 +0500

    init project

---
+++ src/main/java/com/automarket/automarket_service/AutomarketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.automarket.automarket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomarketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomarketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automarket/automarket_service/AutomarketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automarket.automarket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomarketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
