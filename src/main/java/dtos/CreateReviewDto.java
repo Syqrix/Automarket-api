ccb18b603403038530e0ee1e86bca85830dee395
Edward Mironov <edwardmir0515@gmail.com>
2026-06-24 14:02:46 +0500

    added and edit some dtos and create userService

--- src/main/java/dtos/CreateReviewDto.java
+++ src/main/java/dtos/CreateReviewDto.java
@@ -1,12 +1,20 @@
 package dtos;
 
-import entities.Modification;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
 public record CreateReviewDto(
+        @NotBlank(message = "Your should write something")
+        @Size(min = 5, max = 5000, message = "Available range of description between 5 and 5000 symbols")
         String description,
+        @NotNull(message = "Value can't be empty")
+        @PastOrPresent(message = "Your date from future")
         LocalDate ownershipStartDate,
+        @NotNull(message = "Value can't be empty")
+        @Positive(message = "Value should be positive")
         Integer modificationId,
+        @NotNull(message = "Value can't be empty")
+        @Positive(message = "Value should be positive")
         Integer userId
 ) {}
