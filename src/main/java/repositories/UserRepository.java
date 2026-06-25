5661f6ef423186e1fff7b959b455a4256649c81b
Edward Mironov <edwardmir0515@gmail.com>
2026-06-25 12:58:39 +0500

    Improved userService and start working on AdvertisementSevice, created new dto for UserService

--- src/main/java/repositories/UserRepository.java
+++ src/main/java/repositories/UserRepository.java
@@ -2,24 +2,22 @@ package repositories;
 
 import entities.User;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findByUserName(String userName);
+    List<User> findAllUsersByUserName(String userName);
     Optional<User> findByEmail(String email);
-    Optional<User> findByPhoneNumber(String phoneNumber);
-    List<User> findByRole(String role);
+    Optional<User> findByPhoneNumber(String phoneNumber);;
     boolean existsByUserName(String userName);
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
     long countByUserName(String userName);
     Optional<User> findByEmailOrPhoneNumber(String email, String phoneNumber);
-    Page<User> findAll(Pageable pageable);
-    Page<User> findByRole(String role,Pageable pageable);
+    Page<User> findAllUsers(Pageable pageable);
+    Page<User> findAllUsersByRole(String role,Pageable pageable);
 }
